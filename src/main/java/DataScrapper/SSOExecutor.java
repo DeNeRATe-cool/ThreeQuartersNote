735f5852d1b7ac6fc6098ae407923be079879ea6
DeNeRATe-cool <1229836346@qq.com>
2024-11-08 16:57:51 +0800

    finish video downloading

--- src/main/java/DataScrapper/SSOExecutor.java
+++ src/main/java/DataScrapper/SSOExecutor.java
@@ -1,18 +1,15 @@
 package DataScrapper;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.WebElement;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.IOException;
 import java.time.Duration;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class SSOExecutor {
 
@@ -27,16 +24,24 @@ public class SSOExecutor {
     private static final String buttonXPath = "/html/body/div[2]/div/div[3]/div[2]/div[1]/div[7]/input";
     private static final String historyXPath = "/html/body/div/div[2]/div/div/div[1]/div/div[2]/p";
     private static final String timeTableContentXPath = "/html/body/div/div[2]/div/div/div[1]/div[2]/div[3]";
+    private static final String videoSrcXPath = "/html/body/div[1]/div[2]/div/div/div[2]/div[2]/div[1]/div[1]/div[2]/div/div[2]/div[1]/div/div[2]/div[1]/div[1]/video";
 
     private List<WebElement> courseElements;
     private List<WebElement> courseTimeTable;
     private String currentURL;
+    private String videoSource;
+    private List<String> pages = new ArrayList<>();
 
     private String number;
     private String password;
     private String name;
     private String course;
     private String teacher;
+    private String timeTable;
+
+    private ClassListExecutor classListExecutor;
+    private TeacherExecutor teacherExecutor;
+    private TimeTableExecutor timeTableExecutor;
 
     static {
         WebDriverManager.chromedriver().setup();
@@ -73,6 +78,10 @@ public class SSOExecutor {
         return teacher;
     }
 
+    public String getTimeTable() {
+        return timeTable;
+    }
+
     public void setNumber(String number) {
         this.number = number;
     }
@@ -91,6 +100,10 @@ public class SSOExecutor {
 
     public void setTeacher(String teacher) {
         this.teacher = teacher;
+    }
+
+    public void setTimeTable(String timeTable) {
+        this.timeTable = timeTable;
     }
 
     public void initial(String number, String password, String name) {
@@ -130,13 +143,11 @@ public class SSOExecutor {
     /**
      * directly visit searching web for the course
      * @param course target course to search
-     * @param teacher the teacher teaches the course
      * @return true iff search the courses successfully
      * @throws Exception
      */
-    public boolean searchCourse(String course, String teacher) throws Exception {
+    public boolean searchCourse(String course) throws Exception {
         setCourse(course);
-        setTeacher(teacher);
 
         try {
             String targetURL = "https://classroom.msa.buaa.edu.cn/searchContent?title=" + this.course + "&realname=&trans=&page=1&tenant_code=21";
@@ -166,9 +177,11 @@ public class SSOExecutor {
      * List<String> params is like
      * [北京航空航天大学, 计算机硬件基础（软件专业）, 牛建伟]
      * [软件学院, 计算机硬件基础（软件专业）, 邓莹莹]
+     * @param teacher the teacher teaches the course
      * @return true iff gotten the course teaching by the teacher
      */
-    public boolean gotoTargetCourse() {
+    public boolean gotoTargetCourse(String teacher) {
+        setTeacher(teacher);
         for(WebElement element : courseElements) {
             List<String> params = List.of(element.getText().split("\n"));
             System.out.println(params);
@@ -178,12 +191,16 @@ public class SSOExecutor {
                         .executeScript("return document.readyState").equals("complete"));
 
                 currentURL = driver.getWindowHandle();
+                pages.add(currentURL);
+                System.out.println("Current URL: " + currentURL);
                 Set<String> allWindowHandles = driver.getWindowHandles();
 
                 for (String windowHandle : allWindowHandles) {
-                    if (!windowHandle.equals(currentURL)) {
+                    if(!pages.contains(windowHandle)) {
+                        pages.add(windowHandle);
                         driver.switchTo().window(windowHandle);
                         currentURL = driver.getWindowHandle();
+                        System.out.println("Current URL: " + currentURL);
                         break;
                     }
                 }
@@ -201,18 +218,82 @@ public class SSOExecutor {
      * [智播, 第1周星期5第6,7节, 刘子鹏, 未开始]
      * @return true iff get the timetable successfully
      */
-    public boolean targetCourseTimeTable() {
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(timeTableContentXPath)));
-        courseTimeTable = driver.findElements(By.className("content-inner-one"));
+    public boolean targetCourseTimeTable(String timeTable) throws InterruptedException {
+        setTimeTable(timeTable);
+        try {
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(timeTableContentXPath)));
+            courseTimeTable = driver.findElements(By.className("content-inner-one"));
 
-        if(courseElements.isEmpty())
-            return false;
+            if (courseElements.isEmpty())
+                return false;
 
-        for(WebElement element : courseTimeTable) {
-            List<String> params = List.of(element.getText().split("\n"));
-            System.out.println(params);
+            for (WebElement element : courseTimeTable) {
+                List<String> params = List.of(element.getText().split("\n"));
+                if (params.contains(teacher) && params.contains(timeTable)) {
+
+                    element.click();
+                    wait.until(driver -> ((JavascriptExecutor) driver)
+                            .executeScript("return document.readyState").equals("complete"));
+
+                    // MUST wait roughly!!!
+                    Thread.sleep(1000);
+
+                    for (String windowHandle : driver.getWindowHandles()) {
+                        if (!pages.contains(windowHandle)) {
+                            pages.add(windowHandle);
+                            driver.switchTo().window(windowHandle);
+                            currentURL = driver.getWindowHandle();
+                            System.out.println("Current URL: " + currentURL);
+                            break;
+                        }
+                    }
+                    break;
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
         return true;
+    }
+
+    /**
+     * after click to the target url
+     * download the video to the default local path
+     * @return true iff download successfully
+     */
+    public boolean downloadCourseVideo() throws Exception {
+        try {
+            wait.until(driver -> ((JavascriptExecutor) driver)
+                    .executeScript("return document.readyState").equals("complete"));
+            WebElement video = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(videoSrcXPath)));
+            videoSource = video.getAttribute("src");
+
+            try {
+                VideoDownloader.downloadFile(videoSource, course + "_" + teacher + "_" + timeTable + ".mp4");
+                System.out.println("download course video successfully!");
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    public List<String> getTeachers() {
+        teacherExecutor = new TeacherExecutor(courseElements);
+        return teacherExecutor.getTeachers();
+    }
+
+    public List<CourseTimeTable> getCourseTimeTable() {
+        timeTableExecutor = new TimeTableExecutor(courseTimeTable);
+        return timeTableExecutor.getCourseTimeTable();
+    }
+
+    public List<String> getCourseList() {
+        classListExecutor = new ClassListExecutor(driver, wait);
+        return classListExecutor.getCourseList();
     }
 
     public static void main(String[] args) throws Exception {
@@ -220,17 +301,18 @@ public class SSOExecutor {
         String password = "njqyb18283827207";
         String name = "杨佳宇轩";
 
-        String course = "离散数学";
-        String teacher = "张梦豪";
+        String course = "计算机硬件基础（软件专业）";
+        String teacher = "邓莹莹";
+        String timeTable = "第9周星期5第6,7节";
 
         SSOExecutor ssoExecutor = SSOExecutor.getInstance();
         ssoExecutor.initial(number, password, name);
 
         ssoExecutor.login();
 
-
-        ssoExecutor.searchCourse(course, teacher);
-        ssoExecutor.gotoTargetCourse();
-        ssoExecutor.targetCourseTimeTable();
+        ssoExecutor.searchCourse(course);
+        ssoExecutor.gotoTargetCourse(teacher);
+        ssoExecutor.targetCourseTimeTable(timeTable);
+        ssoExecutor.downloadCourseVideo();
     }
 }
